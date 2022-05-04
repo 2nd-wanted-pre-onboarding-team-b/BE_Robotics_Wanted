e2ed89509feaf6f899108d159ad77ee40361ff72
Kiyoon Nam <88444944+Chilla-N@users.noreply.github.com>
2022-05-04 18:38:01 +0900

    Update settings.py

--- point_of_sale/settings.py
+++ point_of_sale/settings.py
@@ -175,19 +175,19 @@ CORS_ALLOW_HEADERS = (
 
 APPEND_SLASH = False
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        }
-    },
-    'loggers': {
-        'django.db.backends': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-        },
-    }
-}
+#LOGGING = {
+#    'version': 1,
+#    'disable_existing_loggers': False,
+#    'handlers': {
+#        'console': {
+#            'level': 'DEBUG',
+#            'class': 'logging.StreamHandler',
+#        }
+#    },
+#    'loggers': {
+#        'django.db.backends': {
+#            'handlers': ['console'],
+#            'level': 'DEBUG',
+#        },
+#    }
+#}

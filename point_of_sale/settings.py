5518475d7fbaec76fde7eccadff36ad8c95d444d
tony <apol.lun058@gmail.com>
2022-05-04 14:56:46 +0900

    add:menu_app_api 1차

--- point_of_sale/settings.py
+++ point_of_sale/settings.py
@@ -171,3 +171,23 @@ CORS_ALLOW_HEADERS = (
     'x-csrftoken',
     'x-requested-with',
 )
+
+
+APPEND_SLASH = False
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#             'level': 'DEBUG',
+#             'class': 'logging.StreamHandler',
+#         }
+#     },
+#     'loggers': {
+#         'django.db.backends': {
+#             'handlers': ['console'],
+#             'level': 'DEBUG',
+#         },
+#     }
+# }

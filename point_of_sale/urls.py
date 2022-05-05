ae2b6f8a0b149bdf15c0568cd9276d3613df9fe9
tony <apol.lun058@gmail.com>
2022-05-06 04:44:43 +0900

    modify: settings 분리 swagger등록

--- point_of_sale/urls.py
+++ point_of_sale/urls.py
@@ -15,6 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title = "BEAR_Robotics",
+        default_version = '0.1.0',
+        description = "",
+        terms_of_service = "",
+    ),
+    public = True,
+    permission_classes = [permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +40,10 @@ urlpatterns = [
     path('api/menu', include('menu.urls')),
     path('api/restaurants', include('restaurants.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += (
+        path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        path(r'swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        path(r'redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
+    )
